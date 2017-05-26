aae1c283e2b456b261c1e7383206d3e294bced7f
Karl O'Keeffe <karl@geckoboard.com>
2017-05-26 21:49:21 +0100

    Extract Filter into it's own folder

--- src/HotelListing/HotelListing.js
+++ src/HotelListing/HotelListing.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
-import { Card, CardHeader, CardText } from 'material-ui/Card';
+import { Card } from 'material-ui/Card';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
-import TextField from 'material-ui/TextField';
-import Checkbox from 'material-ui/Checkbox';
 import ToggleStar from 'material-ui/svg-icons/toggle/star';
-import Slider from 'material-ui/Slider';
 import { yellow600 } from 'material-ui/styles/colors';
+import Filter from '../Filter';
 
 const sortOptions = [
   { id: 1, field: 'Distance', ascending: true, label: 'Distance' },
@@ -35,83 +33,6 @@ const filterHotels = (hotels, filter) => {
       (filter.maxCost === undefined || hotel.MinCost <= filter.maxCost)
     );
   });
-};
-
-const toNumber = value => {
-  if (value === '') {
-    return undefined;
-  }
-  return Number(value);
-};
-
-const Filter = ({ filter, onChange }) => {
-  return (
-    <Card>
-      <CardHeader
-        title="Filter"
-        actAsExpander={true}
-        showExpandableButton={true}
-      />
-      <CardText expandable={true}>
-        <TextField
-          hintText="e.g. Disney"
-          fullWidth={true}
-          floatingLabelText="Hotel Name"
-          value={filter.name}
-          onChange={(event, newName) => onChange({ ...filter, name: newName })}
-        />
-        <div style={{ marginTop: '1rem', marginBottom: '1rem' }}>
-          {[5, 4, 3, 2, 1].map(numStars => (
-            <Checkbox
-              key={numStars}
-              label={
-                <span>
-                  {[...new Array(numStars)].map((value, i) => (
-                    <ToggleStar key={i} color={yellow600} />
-                  ))}
-                </span>
-              }
-              checked={filter.stars[numStars]}
-              onCheck={(event, isChecked) =>
-                onChange({
-                  ...filter,
-                  stars: { ...filter.stars, [numStars]: isChecked },
-                })}
-            />
-          ))}
-        </div>
-        <div>
-          User rating (at least) <b>{filter.userRating.toFixed(1)}</b>
-          <Slider
-            min={0}
-            max={10}
-            value={filter.userRating}
-            onChange={(event, newUserRating) =>
-              onChange({ ...filter, userRating: newUserRating })}
-            style={{ marginBottom: '-48px' }}
-          />
-        </div>
-        <div>
-          <TextField
-            type="number"
-            min="0"
-            floatingLabelText="Min price (£)"
-            value={filter.minCost || ''}
-            onChange={(event, newMinCost) =>
-              onChange({ ...filter, minCost: toNumber(newMinCost) })}
-          />
-          <TextField
-            type="number"
-            min="0"
-            floatingLabelText="Max price (£)"
-            value={filter.maxCost || ''}
-            onChange={(event, newMaxCost) =>
-              onChange({ ...filter, maxCost: toNumber(newMaxCost) })}
-          />
-        </div>
-      </CardText>
-    </Card>
-  );
 };
 
 const Hotel = ({ hotel }) => {
