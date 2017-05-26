840b6132d8ebc460cc767f15a14e5a76a33ff5d4
Karl O'Keeffe <karl@geckoboard.com>
2017-05-26 22:09:58 +0100

    Separate state handling from UI
    State is now handled by App and UI is handled by HotelListing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,87 @@
 import React, { Component } from 'react';
 import HotelListing from './HotelListing';
 
-import './App.css';
+const sortOptions = [
+  { id: 1, field: 'Distance', ascending: true, label: 'Distance' },
+  { id: 2, field: 'Stars', descending: false, label: 'Stars' },
+  { id: 3, field: 'MinCost', ascending: true, label: 'Price (lowest first)' },
+  { id: 4, field: 'MinCost', ascending: false, label: 'Price (highest first)' },
+  { id: 5, field: 'UserRating', ascending: false, label: 'Rating' },
+];
+
+const sortHotels = (hotels, sortBy) => {
+  return [...hotels].sort((a, b) => {
+    const valueA = a[sortBy.field];
+    const valueB = b[sortBy.field];
+
+    return sortBy.ascending ? valueA - valueB : valueB - valueA;
+  });
+};
+
+const filterHotels = (hotels, filter) => {
+  return hotels.filter(hotel => {
+    return (
+      hotel.Name.toLowerCase().includes(filter.name.toLowerCase()) &&
+      filter.stars[hotel.Stars] &&
+      hotel.UserRating >= filter.userRating &&
+      (filter.minCost === undefined || hotel.MinCost >= filter.minCost) &&
+      (filter.maxCost === undefined || hotel.MinCost <= filter.maxCost)
+    );
+  });
+};
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hotels: [],
+      loading: true,
+      sortBy: sortOptions[0],
+      filter: {
+        name: '',
+        stars: {
+          5: true,
+          4: true,
+          3: true,
+          2: true,
+          1: true,
+        },
+        userRating: 0,
+        minCost: undefined,
+        maxCost: undefined,
+      },
+    };
+  }
+
+  async componentDidMount() {
+    const result = await fetch('./hotels.json');
+    const data = await result.json();
+    this.setState({
+      hotels: data.Establishments,
+      loading: false,
+    });
+  }
+
+  handleSortChange(id) {
+    const sortBy = sortOptions.find(f => f.id === id);
+    this.setState({ sortBy });
+  }
+
   render() {
+    const { hotels: allHotels, sortBy, filter } = this.state;
+    const hotels = sortHotels(filterHotels(allHotels, filter), sortBy).slice(
+      0,
+      10,
+    );
     return (
-      <HotelListing />
+      <HotelListing
+        hotels={hotels}
+        sortOptions={sortOptions}
+        sortBy={sortBy}
+        filter={filter}
+        onFilterChange={newFilter => this.setState({ filter: newFilter })}
+        onSortChange={id => this.handleSortChange(id)}
+      />
     );
   }
 }
--- src/HotelListing/HotelListing.js
+++ src/HotelListing/HotelListing.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import AppBar from 'material-ui/AppBar';
 import Filter from '../Filter';
 import SortBy from '../SortBy';
@@ -6,101 +6,33 @@ import Hotel from '../Hotel';
 
 import './HotelListing.css';
 
-const sortOptions = [
-  { id: 1, field: 'Distance', ascending: true, label: 'Distance' },
-  { id: 2, field: 'Stars', descending: false, label: 'Stars' },
-  { id: 3, field: 'MinCost', ascending: true, label: 'Price (lowest first)' },
-  { id: 4, field: 'MinCost', ascending: false, label: 'Price (highest first)' },
-  { id: 5, field: 'UserRating', ascending: false, label: 'Rating' },
-];
-
-const sortHotels = (hotels, sortBy) => {
-  return [...hotels].sort((a, b) => {
-    const valueA = a[sortBy.field];
-    const valueB = b[sortBy.field];
-
-    return sortBy.ascending ? valueA - valueB : valueB - valueA;
-  });
-};
-
-const filterHotels = (hotels, filter) => {
-  return hotels.filter(hotel => {
-    return (
-      hotel.Name.toLowerCase().includes(filter.name.toLowerCase()) &&
-      filter.stars[hotel.Stars] &&
-      hotel.UserRating >= filter.userRating &&
-      (filter.minCost === undefined || hotel.MinCost >= filter.minCost) &&
-      (filter.maxCost === undefined || hotel.MinCost <= filter.maxCost)
-    );
-  });
-};
-
-class HotelListing extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      hotels: [],
-      loading: true,
-      sortBy: sortOptions[0],
-      filter: {
-        name: '',
-        stars: {
-          5: true,
-          4: true,
-          3: true,
-          2: true,
-          1: true,
-        },
-        userRating: 0,
-        minCost: undefined,
-        maxCost: undefined,
-      },
-    };
-  }
-
-  async componentDidMount() {
-    const result = await fetch('./hotels.json');
-    const data = await result.json();
-    this.setState({
-      hotels: data.Establishments,
-      loading: false,
-    });
-  }
-
-  handleSortChange(id) {
-    const sortBy = sortOptions.find(f => f.id === id);
-    this.setState({ sortBy });
-  }
-
-  render() {
-    const { hotels, sortBy, filter } = this.state;
-    const results = sortHotels(filterHotels(hotels, filter), sortBy).slice(
-      0,
-      10,
-    );
-    return (
+const HotelListing = ({
+  hotels,
+  sortOptions,
+  sortBy,
+  filter,
+  onFilterChange,
+  onSortChange,
+}) => {
+  return (
+    <div>
+      <AppBar title="Hotel Motel Holiday Inn" showMenuIconButton={false} />
       <div>
-        <AppBar title="Hotel Motel Holiday Inn" showMenuIconButton={false} />
+        <h3 className={'header'}>Hotels in Paris</h3>
+        <Filter filter={filter} onChange={onFilterChange} />
+        <SortBy
+          sortOptions={sortOptions}
+          selected={sortBy}
+          onChange={onSortChange}
+        />
         <div>
-          <h3 className={'header'}>Hotels in Paris</h3>
-          <Filter
-            filter={filter}
-            onChange={newFilter => this.setState({ filter: newFilter })}
-          />
-          <SortBy
-            sortOptions={sortOptions}
-            selected={sortBy}
-            onChange={id => this.handleSortChange(id)}
-          />
-          <div>
-            {results.map(hotel => (
-              <Hotel key={hotel.EstablishmentId} hotel={hotel} />
-            ))}
-          </div>
+          {hotels.map(hotel => (
+            <Hotel key={hotel.EstablishmentId} hotel={hotel} />
+          ))}
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default HotelListing;
